e2eef5ec9ce094b83f9521264458cffdbde005a8
liyuji <fackbing@hotmail.com>
2023-07-03 11:06:15 +0800

    add files

---
+++ initialization.js
@@ -0,0 +1,28 @@
+﻿// 编辑器选项
+const options = {
+	language: 'zh',
+	mode: 'code',
+	modes: ['text', 'code', 'form', 'tree', 'view'], // allowed modes
+	enableSort: false,
+	onError: function (err) {
+		//alert(err.toString())
+	},
+	onModeChange: function (newMode, oldMode) {
+		//console.log('Mode switched from', oldMode, 'to', newMode)
+	}
+}
+
+// 创建编辑器
+var editor = new JSONEditor(document.getElementById("jsonEditor"), options);
+
+// 设置默认数据
+editor.set({
+	'array': [1, 2, 3],
+	'boolean': true,
+	'color': '#82b92c',
+	'null': null,
+	'number': 123,
+	'object': {'a': 'b', 'c': 'd'},
+	'string': 'Hello World'
+});
+
